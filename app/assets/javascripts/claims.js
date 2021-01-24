46f1ec02a7ce639a075c336fd0a2293e58fd26e4
yuta-hanyu <remakeremake9011@gmail.com>
2021-01-24 12:30:23 +0000

    date_select修正

---
+++ app/assets/javascripts/claims.js
@@ -0,0 +1,33 @@
+// document.addEventListener('DOMContentLoaded', function() {
+  
+// const app = new Vue({
+//   el: '#example',
+//   data: {
+//     i_date: '',
+//     e_date: '',
+//   },
+//   computed: {
+//     date() {
+//       if (this.i_date && this.e_date) {
+
+//         // BMIを計算する
+//         const date = this.i_date + this.e_date;
+
+//         return date;
+//       }
+
+//       return '';
+//     },
+//   },
+// });
+  
+// });
+
+// $(function() {
+  
+//   $('.my-input').on('input', (e) => {
+//     const value = $(e.target).val();
+//     $('.box').text(value);
+//   });
+  
+// });
