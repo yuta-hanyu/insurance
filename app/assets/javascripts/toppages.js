224fecf56a4fc1b43c468775cf1fdc452368da61
yuta-hanyu <remakeremake9011@gmail.com>
2021-01-31 08:33:53 +0000

    スライドショー

--- app/assets/javascripts/toppages.js
+++ app/assets/javascripts/toppages.js
@@ -138,6 +138,37 @@ $(function() {
  
   });
 });
+// スライドショー
+$('.likes').slick({
+  arrows: true,
+  prevArrow:'<div class="prev"><i class="fas fa-angle-double-left"></i></div>',
+  nextArrow:'<div class="next"><i class="fas fa-angle-double-right"></i></div>',
+  autoplay: true,
+  autoplaySpeed: 700,
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
 
 });
 
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,7 +3,7 @@ class ToppagesController < ApplicationController
 
   
   def index
-    @posts = Post.order(id: :desc).page(params[:page]).per(4)
+    @posts = Post.order(id: :desc)
   end
   
   def show
