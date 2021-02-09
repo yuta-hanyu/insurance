eef54a74ea816028a45bdc408eb501b9213e8c79
yuta-hanyu <remakeremake9011@gmail.com>
2021-02-09 15:42:24 +0000

    post ajax

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -124,4 +124,14 @@ $(function() {
     }
   });
 });
+// 詳細表示切替 
+$(function() {
+    // フォームを開く
+    // リロードしないと表示されなくなるため、documentを付与
+  $(document).on("click", "#e_contract", function () {
+    const contractId = $(this).data('contract-id');
+    const contractTextArea = $('#contract_e-' + contractId);
+    contractTextArea.toggle('slow');
+  });
+});
 
