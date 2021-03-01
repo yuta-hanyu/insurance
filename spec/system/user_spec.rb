2c2c8b09466f33af648131e7d8270e4412ae2ec0
yuta-hanyu <remakeremake9011@gmail.com>
2021-03-01 14:54:30 +0000

    test user delete

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -49,16 +49,10 @@ RSpec.describe "新規登録テスト", type: :system do
       click_on '登録'
       expect(page).to have_content 'ご契約情報の登録が完了しました'
     end
-    # it '新規登録失敗か' do
-    #   visit root_path
-    #   expect(page).to have_content('新規登録')
-    #   fill_in "user[contractor_name]", with: ""
-    #   fill_in "user[email]", with: @user.email
-    #   fill_in "user[password]", with: @user.password
-    #   fill_in "user[password_confirmation]", with: @user.email
-    #   fill_in 'user[address]', with: @user.address
-    #   click_on '登録'
-    #   expect(page).to have_content '契約者名を入力してください'
-    # end
+    it '退会できるか' do
+      visit "/users/delete/confirm/#{@new_user.id}"
+      expect(page).to have_content('退会のご確認')
+      expect{ @new_user.destroy }.to change{ User.count }.by(-1)
+    end
  end
 end
